df2a6d0ff9731784d7933670358a5fb4b9a0bc0f
odalis-rea-c <osxyrea@gmail.com>
2026-04-19 21:25:06 -0500

    Services

---
+++ frontend/src/app/services/task.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl = 'http://127.0.0.1:8000/api/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  updateStatus(id: number, status: string): Observable<Task> {
+    return this.http.patch<Task>(`${this.apiUrl}/${id}/status`, { status });
+  }
+
+  deleteTask(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
