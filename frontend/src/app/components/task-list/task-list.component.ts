bdc60aa0961c0a54a3ff1cb31386782ecb51cc35
odalis-rea-c <osxyrea@gmail.com>
2026-04-19 21:25:06 -0500

    Componente Lista

---
+++ frontend/src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task';
+
+@Component({
+  selector: 'app-task-list',
+  imports: [],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.css'
+})
+export class TaskListComponent implements OnInit{
+   tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe(data => {
+      this.tasks = data;
+    });
+  }
+
+  changeStatus(task: Task) {
+    const nextStatus =
+      task.status === 'TODO' ? 'IN_PROGRESS' :
+      task.status === 'IN_PROGRESS' ? 'DONE' : 'TODO';
+
+    this.taskService.updateStatus(task.id!, nextStatus)
+      .subscribe(() => this.loadTasks());
+  }
+
+  deleteTask(id: number) {
+    this.taskService.deleteTask(id)
+      .subscribe(() => this.loadTasks());
+  }
+}
+
+
